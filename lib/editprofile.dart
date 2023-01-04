69e7a208ad2707e17231142d1055ff550811d3b2
Abdullah <abdullhalshmas@gmail.com>
2023-01-04 15:41:58 +0300

    c

--- lib/editprofile.dart
+++ lib/editprofile.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'cards.dart';
 import 'profile.dart';
 
@@ -14,10 +15,19 @@ class Editprofile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<Editprofile> {
+  final _firestore = FirebaseFirestore.instance;
   bool showPassword = false;
   var userEmail = FirebaseAuth.instance.currentUser?.email;
   var userNAME = FirebaseAuth.instance.currentUser?.displayName;
   var userphone = FirebaseAuth.instance.currentUser?.phoneNumber;
+  var newname = FirebaseAuth.instance.currentUser?.displayName;
+  var newpass;
+  var newphone;
+  var newjob;
+  var newaddress;
+  var newskill;
+  var newhoobie;
+  String? jobtitle;
 
   final _auth = FirebaseAuth.instance;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
@@ -31,35 +41,78 @@ class _EditProfileState extends State<Editprofile> {
             child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('images/profile.png'),
-                radius: 50,
-              ),
+            CircleAvatar(
+              radius: 58,
+              backgroundImage: const AssetImage("images/profile.png"),
+              child: Stack(children: const [
+                Align(
+                  alignment: Alignment.topRight,
+                  child: CircleAvatar(
+                    radius: 12,
+                    backgroundColor: Color.fromARGB(179, 255, 255, 255),
+                    child: Icon(
+                      Icons.flip_camera_ios,
+                      size: 20,
+                    ),
+                  ),
+                ),
+              ]),
             ),
             const SizedBox(height: 30),
-            const Divider(
-              thickness: 2,
-              height: 60,
-              color: Color.fromARGB(255, 250, 223, 103),
-            ),
             const SizedBox(height: 30),
-            buildTextField("Email", "$userEmail", false),
             buildTextField("Name", "$userNAME", false),
             buildTextField("Password", "", true),
-            buildTextField("Phone", showwidget(), false),
-            const SizedBox(height: 30),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                FirebaseAuth.instance.currentUser?.updateDisplayName(newname);
+                if (newpass != null) {
+                  FirebaseAuth.instance.currentUser?.updatePassword(newpass);
+                }
+              },
               style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 7, 7, 7),
-                padding: EdgeInsets.symmetric(horizontal: 50),
+                backgroundColor: const Color.fromARGB(255, 7, 7, 7),
+                padding: const EdgeInsets.symmetric(horizontal: 50),
                 elevation: 2,
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(20)),
               ),
-              child: Text(
-                "SAVE",
+              child: const Text(
+                "UPDATE",
+                style: TextStyle(
+                    fontSize: 14, letterSpacing: 2.2, color: Colors.white),
+              ),
+            ),
+            const Divider(
+              thickness: 2,
+              height: 40,
+              color: Color.fromARGB(255, 250, 223, 103),
+            ),
+            const SizedBox(height: 30),
+            buildTextField("Phone", '', false),
+            buildTextField("Address", '', false),
+            buildTextField("Job Title", '', false),
+            buildTextField("Skills", '', false),
+            buildTextField("Hobbies", '', false),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                _firestore.collection('profile').add({
+                  'address': newaddress,
+                  'hobbie': newhoobie,
+                  'jobtitle': newjob,
+                  'phonenumber': newphone,
+                  'skill': newskill,
+                });
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 7, 7, 7),
+                padding: const EdgeInsets.symmetric(horizontal: 50),
+                elevation: 2,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+              child: const Text(
+                "ADD",
                 style: TextStyle(
                     fontSize: 14, letterSpacing: 2.2, color: Colors.white),
               ),
@@ -78,25 +131,12 @@ class _EditProfileState extends State<Editprofile> {
     }
   }
 
-  Widget _backButton() {
-    return InkWell(
-      onTap: () {
-        Navigator.pop(context);
-      },
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Row(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.only(left: 0, top: 10, bottom: 10),
-              child: const Icon(Icons.keyboard_arrow_left, color: Colors.black),
-            ),
-            const Text('Back',
-                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500))
-          ],
-        ),
-      ),
-    );
+  String jobtitles() {
+    if (_firestore.collection('profile') == null) {
+      return "PhoneNumber is Empty";
+    } else {
+      return '$userphone';
+    }
   }
 
   Widget buildTextField(
@@ -104,6 +144,29 @@ class _EditProfileState extends State<Editprofile> {
     return Padding(
       padding: const EdgeInsets.only(bottom: 35.0),
       child: TextField(
+        onChanged: (value) {
+          if (labelText == 'Name' && value != '') {
+            newname = value;
+          }
+          if (labelText == 'Password' && value != '') {
+            newpass = value;
+          }
+          if (labelText == 'Phone' && value != '') {
+            newphone = value;
+          }
+          if (labelText == 'Address' && value != '') {
+            newaddress = value;
+          }
+          if (labelText == 'Job Title' && value != '') {
+            newjob = value;
+          }
+          if (labelText == 'Skills' && value != '') {
+            newskill = value;
+          }
+          if (labelText == 'Hobbies' && value != '') {
+            newhoobie = value;
+          }
+        },
         obscureText: isPasswordTextField ? showPassword : false,
         decoration: InputDecoration(
             suffixIcon: isPasswordTextField
@@ -131,7 +194,7 @@ class _EditProfileState extends State<Editprofile> {
             hintStyle: const TextStyle(
               fontSize: 16,
               fontWeight: FontWeight.bold,
-              color: Colors.black,
+              color: Colors.grey,
             )),
       ),
     );
